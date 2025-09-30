b803073f783c2ae51ea919441081bd88f38e9719
Taehoon Kim <95288696+goathoon@users.noreply.github.com>
2025-09-30 23:13:59 +0900

    [level 3] Title: 주사위 고르기, Time: 720.24 ms, Memory: 148 MB -BaekjoonHub

--- 프로그래머스/3/258709. 주사위 고르기/주사위 고르기.java
+++ 프로그래머스/3/258709. 주사위 고르기/주사위 고르기.java
@@ -1,129 +1,91 @@
-import java.io.*;
 import java.util.*;
-
 class Solution {
+    static int[] answer;
+    static int[][] diceA;
+    static int[][] diceB;
+    static int a = 0;
+    static int b = 0;
+    static Set<Integer> choiceA = new HashSet<>();
+    static int max = 0;
     
-    // 주사위 선택
-    static void pickDice(int cnt, int start, int[][] dice) {
-        
-        if(cnt == N / 2) {
-            
-            int winCount = calculateWin(dice);
-            
-            if(max < winCount) {
-                max = winCount;
-                
-                for(int i = 0; i < selected.size(); i++) {
-                    result[i] = selected.get(i) + 1;
-                }
+    // 주사위 고르기
+    public void pickDiceAndSum(int n, int depth, int idx, int[][] dice) {
+        if(n == depth) {
+            // 고른 주사위에서 하나씩 고른 합을 배열에 저장 (A, B)
+            for(int i = 0; i < dice.length; i++){
+                if(!choiceA.contains(i)){
+                    diceB[b++] = dice[i];
+                } 
             }
-            
+            calculateWinning(diceA,diceB);
+            b = 0;
             return;
         }
         
-        for(int i = start; i < N; i++) {
-            
-            selected.add(i);
-            pickDice(cnt+1, i+1, dice);
-            selected.remove(selected.size()-1);
+        for(int i = idx; i < dice.length; i++) {
+            choiceA.add(i);
+            diceA[a++] = dice[i];
+            pickDiceAndSum(n, depth+1, i+1, dice);
+            diceA[--a] = null;
+            choiceA.remove(i);
+            // pickDiceAndSum(n, depth, i+1, dice);
         }
         
     }
     
-    static int calculateWin(int[][] dice) {
+    public void calculateWinning(int[][] diceA, int[][] diceB) {
+        // A배열의 각 원소를 순회하며 B배열의 원소보다 큰 횟수를 계산 (승리 횟수)  
+        List<Integer> dicesA = new ArrayList<>();
+        recursiveDice(0,0,diceA,dicesA);
         
-        int count = 0;
+        List<Integer> dicesB = new ArrayList<>();
+        recursiveDice(0,0,diceB,dicesB);
         
-        makeArrAB(dice);
+        int win = 0;
         
-        // arrA, arrB에 대하여 승패를 결정한다.
-        
-        Collections.sort(arrB);
-        
-        // 승부를 결정할 때, 이분 탐색을 통하여 시간복잡도를 줄인다. O(6^5 * log 6^5)
-        
-        for(int i = 0; i < arrA.size(); i++) {
-            
-            int target = arrA.get(i);
-            
-            int st = 0;
-            int et = arrB.size() - 1;
-            
-            int idx = -1;
-            while(st <= et) {
-                
-                int mid = (st + et) >> 1;
-                
-                if(target <= arrB.get(mid)) {
-                    et = mid - 1;
+        Collections.sort(dicesB);
+        for(int a : dicesA) {
+            int l = 0;
+            int r = dicesB.size();
+            while(l < r) {
+                int mid = (l+r)/2;
+                if(a > dicesB.get(mid)){
+                    l = mid+1;
                 } else {
-                    idx = Math.max(idx, mid);
-                    st = mid + 1;
+                    r = mid;
                 }
-                
             }
-            if(idx != -1) count += idx + 1;
+            win += l;
+            max = Math.max(max,win);
+            
+            int idx = 0;
+            if(max == win) {
+                for(int x : choiceA) {
+                    answer[idx++] = x+1;
+                }
+            }
             
         }
-        
-        return count;
     }
     
-    
-    // 선택된 주사위들의 합 분포를 저장
-    static void sumDice(int cnt, int sum, int[][] dice, List<Integer> arr) {
-        
-        if(cnt == N / 2) {
-            arr.add(sum);
+    // 선택한 주사위의 눈의 합을 저장
+    public void recursiveDice(int depth, int sum, int[][] dice, List<Integer> dices) {
+        if (dice.length == depth) {
+            dices.add(sum);
             return;
         }
-        
         for(int i = 0; i < 6; i++) {
-            int newSum = sum + dice[cnt][i];
-            sumDice(cnt+1, newSum, dice, arr);
-        }   
-    }
-    
-    static void makeArrAB(int[][] dice) {
-        
-        // 선택된 주사위들의 합 분포를 저장한 배열
-        arrA = new ArrayList<>();
-        arrB = new ArrayList<>();
-        
-        int[][] diceA = new int[N / 2][6];
-        int[][] diceB = new int[N / 2][6];
-        
-        int a = 0, b = 0;
-        for(int i = 0; i < N; i++) {
-            
-            if(selected.contains(i)) {
-                diceA[a++] = dice[i];
-            } else {
-                diceB[b++] = dice[i];
-            }
-            
+            recursiveDice(depth+1, sum + dice[depth][i], dice, dices);
         }
-        
-        sumDice(0, 0, diceA, arrA);
-        sumDice(0, 0, diceB, arrB);
-        
     }
     
     
-    
-    static int N, max = 0;
-    static int[] result;
-    static List<Integer> selected, arrA, arrB;
     public int[] solution(int[][] dice) {
-        N = dice.length;
-        result = new int[N / 2];
-        
-        selected = new ArrayList<>();
-        
-        pickDice(0, 0, dice);
-        
-        
-        return result;
-        
+        diceA = new int[dice.length/2][6];
+        diceB = new int[dice.length/2][6];
+        answer = new int[dice.length/2];
+        pickDiceAndSum(dice.length/2, 0, 0, dice);
+        Arrays.sort(answer);
+        return answer;
     }
 }
