e32f267928dacecd9f3709d4f366d8f12264e0b2
Taehoon Kim <95288696+goathoon@users.noreply.github.com>
2026-01-28 00:11:16 +0900

    [level 2] Title: 택배 배달과 수거하기, Time: 17.73 ms, Memory: 81.8 MB -BaekjoonHub

--- 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.java
+++ 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.java
@@ -1,95 +1,49 @@
-import java.util.*;
-
 class Solution {
     public long solution(int cap, int n, int[] deliveries, int[] pickups) {
-        int allD = 0;
-        int lastIdxD = -1;
-        int allP = 0;
-        int lastIdxP = -1;
-        
-        for(int i = 0; i < deliveries.length; i++) {
-            if(deliveries[i] != 0) {
-                lastIdxD = i;
-                allD += deliveries[i];
-            }
-        }
-        
-        for(int i = 0; i < pickups.length; i++) {
-            if(pickups[i] != 0) {
-                lastIdxP = i;
-                allP += pickups[i];
-            }
-        }
-        
+        int endD = -1;
+        int endP = -1;
         long answer = 0;
         
-        while(true) {
-            if(allD == 0 && allP == 0) {
-                break;
+        for(int i = 0; i < n; i++) {
+            if(deliveries[i] > 0) {
+                endD = i;
+            } if (pickups[i] > 0) {
+                endP = i;
             }
+        }
+        
+        
+        while(!(endD == -1 && endP == -1)) {
+            answer += (Math.max(endD, endP) + 1) * 2;
             
-            if(lastIdxD >= lastIdxP) {
-                answer += 2 * (lastIdxD+1);
-                
-                int sumD = 0;
-                int sumP = 0;
-                for(int s = lastIdxD; s >= 0; s--) {
-                    if(deliveries[s] + sumD > cap) {
-                        deliveries[s] -= (cap - sumD);
-                        allD -= (cap - sumD);
-                        break;
-                    } else {
-                        allD -= deliveries[s];
-                        sumD += deliveries[s];
-                        deliveries[s] = 0;    
-                        lastIdxD--;
-                    }
-                }
-                for(int s = lastIdxP; s >= 0; s--) {
-                   if(pickups[s] + sumP > cap) {
-                        pickups[s] -= (cap - sumP);
-                        allP -= (cap - sumP);
-                        break;
-                    } else {
-                        allP -= pickups[s];
-                        sumP += pickups[s];
-                        pickups[s] = 0;
-                        lastIdxP--;
-                    } 
-                }
-                    
+            int deliver = cap;
+            int pickup = cap;
             
-            } else {
-                answer += 2 * (lastIdxP+1);
-                
-               int sumD = 0;
-                int sumP = 0;
-                for(int s = lastIdxD; s >= 0; s--) {
-                    if(deliveries[s] + sumD > cap) {
-                        deliveries[s] -= (cap - sumD);
-                        allD -= (cap - sumD);
-                        break;
-                    } else {
-                        allD -= deliveries[s];
-                        sumD += deliveries[s];
-                        deliveries[s] = 0;
-                        lastIdxD--;
-                    }
+            for(int i = endD; i >= 0; i--) {
+                if(deliveries[i] > deliver) {
+                    deliveries[i] -= deliver;
+                    deliver = 0;
+                    break;
                 }
-                for(int s = lastIdxP; s >= 0; s--) {
-                   if(pickups[s] + sumP > cap) {
-                        pickups[s] -= (cap - sumP);
-                        allP -= (cap - sumP);
-                        break;
-                    } else {
-                        allP -= pickups[s];
-                        sumP += pickups[s];
-                        pickups[s] = 0;
-                        lastIdxP--;
-                    } 
+                else {
+                    deliver -= deliveries[i];
+                    deliveries[i] = 0;
+                    endD--;
                 }
             }
-           
+            for(int i = endP; i >= 0; i--) {
+                if(pickups[i] > pickup) {
+                    pickups[i] -= pickup;
+                    pickup = 0;
+                    break;
+                }
+                else {
+                    pickup -= pickups[i];
+                    pickups[i] = 0;
+                    endP--;
+                }
+            }
+            
         }
         return answer;
     }
